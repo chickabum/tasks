8dac68be5d3246694d04df028c296d4e2c5458e0
chickabum <annadyba90@gmail.com>
2017-05-24 16:16:13 +0200

    wyświetla największą liczbę

---
+++ task2.js
@@ -0,0 +1,13 @@
+//największa z 3 liczb
+
+var a = 12;
+var b = 13;
+var c = 14;
+
+if ((a > b) && (a < c)) {
+    console.log(a)
+} else if ((b > c) && (b > a)) {
+    console.log(b)
+} else {
+    console.log(c)
+}
