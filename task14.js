79865ca8b5d9ad8d58cf79cd53cfd18b93dd9d0f
chickabum <annadyba90@gmail.com>
2017-05-25 12:21:53 +0200

    wypisuje n razy

---
+++ task14.js
@@ -0,0 +1,11 @@
+//zad 14 wypisuje n razy
+
+
+function wypisz(n) {
+
+    var n = prompt("podaj liczbę napisów");
+
+    for (var i = 0; i < n; i++) {
+        console.log(i + 1 + " Programowanie w PHP jest faj");
+    }
+}
